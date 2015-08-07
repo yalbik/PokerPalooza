8d5d8b76d720c02220cd6561a162861bb06518ce
Scott Stephenson <yalbik@yahoo.com>
2015-08-07 15:57:51 -0700

    first tests now we just need a db

---
+++ pokerpalooza.domain/Repo/pokerpalooza-repo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Dapper;
+using DapperExtensions;
+
+using pokerpalooza.domain.Models;
+
+namespace pokerpalooza.domain.Repo
+{
+    public class pokerpalooza_repo
+    {
+        public SqlConnection Connection { get; set; }
+
+        public pokerpalooza_repo(string connectionString)
+        {
+            Connection = new SqlConnection(connectionString);
+        }
+
+        public void InsertBlind(Blind blind)
+        {
+            Connection.Insert<Blind>(blind);
+        }
+
+        public void UpdateBlind(Blind blind)
+        {
+            Connection.Update<Blind>(blind);
+        }
+
+        public void DeleteBlind(Blind blind)
+        {
+            Connection.Delete<Blind>(blind);
+        }
+    }
+}
--- pokerpalooza.tests/repo/repotests.cs
+++ pokerpalooza.tests/repo/repotests.cs
@@ -1,16 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using NUnit.Framework;
 
+using pokerpalooza.domain.Repo;
+using pokerpalooza.domain.Models;
+
 namespace pokerpalooza.tests.repo
 {
     [TestFixture]
     public class repotests
     {
-        
+        pokerpalooza_repo repo;
+
+        [TestFixtureSetUp]
+        public void SetShitUp()
+        {
+            repo = new pokerpalooza_repo(ConfigurationManager.ConnectionStrings["pokerpalooza-db"].ToString());
+        }
+
+        [Test]
+        public void CanGetARepo()
+        {
+            Assert.IsNotNull(repo);
+        }
+
+        [Test]
+        public void Crud_Blind()
+        {
+            Blind blind = new Blind()
+            {
+                ID = 0,
+                BlindSetupID = 9999,
+                BlindLevel = 100,
+                Interval = new TimeSpan(0, 20, 0)
+            };
+
+            repo.InsertBlind(blind);
+        }
     }
 }

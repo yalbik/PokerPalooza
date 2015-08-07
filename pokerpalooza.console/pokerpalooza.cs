c37fa233dbd7f7773118e8f3992f71e0a05f820c
Scott Stephenson <yalbik@yahoo.com>
2015-08-07 15:03:00 -0700

    added console app

---
+++ pokerpalooza.console/pokerpalooza.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokerpalooza.console
+{
+    class pokerpalooza
+    {
+        public pokerpalooza()
+        {
+            Console.WriteLine("shit works.");
+        }
+
+        public static void Main(String[] args)
+        {
+            new pokerpalooza();
+        }
+    }
+}
